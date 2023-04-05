a4b51656aee1bcedcb8995443d4b058b24bb5881
ambujsahu81 <Ambuj.sahu1998@gmail.com>
2023-04-05 18:21:32 +0530

    form validation is completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { EventRegistrationComponent } from './event-registration/event-registration.component';
 import { EventDetailsComponent } from './event-details/event-details.component';
@@ -14,7 +15,8 @@ import { EventDetailsComponent } from './event-details/event-details.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/event-registration/event-registration.component.ts
+++ src/app/event-registration/event-registration.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-event-registration',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./event-registration.component.css']
 })
 export class EventRegistrationComponent {
+  shwFirstNameError = false;
+  shwEmailError = false;
+  registrationForm = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: [''],
+    email: ['', Validators.email],
+    jobTitle: [''],
+    companyName: [''],
+    companySize: [''],
+    message: [''],
+    futureEmails: false
+  });
 
+  constructor(private fb: FormBuilder) { }
+
+  onSubmit() {
+    if (this.registrationForm.valid) {
+      this.shwEmailError = false;
+      this.shwFirstNameError = false;
+      let formData = JSON.stringify(this.registrationForm.value, null, 4)
+      let message = `Hello ${this.registrationForm.value.firstName}, Thankyou, registration is successfully completed \n\n ${formData} `;
+      alert(message);
+    } else if ( this.registrationForm.controls.firstName.invalid) {
+      this.shwFirstNameError = true;
+    } else if ( this.registrationForm.controls.email.invalid ) {
+      this.shwEmailError = true; 
+    }  
+  }
 }
