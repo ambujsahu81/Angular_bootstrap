00a649b9a735358de4a06f4d1bbfe1fa0de39498
ambujsahu81 <Ambuj.sahu1998@gmail.com>
2023-04-05 14:32:24 +0530

    added simple bootstrap form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EventRegistrationComponent } from './event-registration/event-registration.component';
+import { EventDetailsComponent } from './event-details/event-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventRegistrationComponent,
+    EventDetailsComponent
   ],
   imports: [
     BrowserModule,
